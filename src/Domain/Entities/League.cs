4bed01c538977ca574282a458d670ba16e23cc50
Newton <ntkyari@gmail.com>
2022-08-27 11:02:32 +0100

    Remove not relating files

--- src/Domain/Entities/League.cs
+++
@@ -1,8 +0,0 @@
-namespace Domain.Entities;
-public class League
-{
-    public Guid Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-    public string ShortName { get; set; } = string.Empty;
-    
-}
--- src/Domain/Entities/Stadium.cs
+++
@@ -1,9 +0,0 @@
-namespace Domain.Entities;
-
-public class Stadium
-{
-    public Guid Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-    public string? City { get; set; }
-    public int Capacity { get; set; }
-}
